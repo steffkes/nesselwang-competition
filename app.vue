c6edb4d8be6986ef016669f96db7b401749c08ee
steffkes <stefan@gelungen.es>
2025-05-08 17:21:56 +0200

    setup basic page

--- app.vue
+++ app.vue
@@ -1,6 +1,81 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="container">
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/">
+            <strong>Nesselwang Competition</strong>
+          </a>
+
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+            data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <!--
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-primary">
+                  <strong>Sag mir auf jeden Fall Bescheid!</strong>
+                </a>
+              </div>
+            </div>
+          </div>
+          -->
+        </div>
+      </nav>
+    </div>
+
+    <section class="section">
+      <div class="container">
+        <!--
+        <div class="notification is-primary">
+          This container is <strong>centered</strong> on desktop and larger viewports.
+        </div>
+        -->
+
+        <div style="position:relative; width:100%; height:0px; padding-bottom:56.250%">
+          <iframe allow="fullscreen;autoplay" allowfullscreen height="100%"
+            src="https://streamable.com/e/sksaz8?autoplay=1&loop=0" width="100%"
+            style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;">
+          </iframe>
+        </div>
+
+      </div>
+    </section>
+
+
+    <footer class="footer">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <div class="content">
+              <p><strong>Nesselwang Competition</strong> wird organisiert von Stefan Matheis, dem Veranstalter von <a
+                  href="https://www.schachmatt-ulm.de">Schachmatt Ulm</a> & Initiator des Schanzenlauf
+                Oberstdorf. </p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="columns">
+              <div class="column is-4"><img src="/images/stefan.jpg" alt="Bild Stefan Matheis"></div>
+              <div class="column">
+                <p>Fragen? Anregungen? Presse. Sponsoring.</p>
+                <p class="mt-4"><strong>Stefan Matheis</strong><br><a href="mailto:stefan@mathe.is">✉️
+                    stefan@mathe.is</a><br><a href="tel:004916097048114">📞 +49 160 970 48 114</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+
   </div>
 </template>
