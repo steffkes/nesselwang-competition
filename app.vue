2d06c15ccd360d7dace7d95ad5e63f0841b70545
steffkes <stefan@gelungen.es>
2025-05-12 23:14:32 +0200

    add explanation text for competition

--- app.vue
+++ app.vue
@@ -75,7 +75,7 @@ useHead({
       ><img src="/qr-code.svg" style="opacity: 0.1; height: 20px; margin: 10px"
     /></a>
 
-      <section
+    <section
       class="section"
       style="
         min-height: 50vh;
@@ -84,16 +84,27 @@ useHead({
         background-position: 50% 65%;
       "
     >
-    <div class="container">
+      <div class="container">
         <div class="columns">
           <div class="column is-two-fifths">
             <a href="/"><img src="/nesselwang-competition.svg" alt="Logo Nesselwang Competition"></a>
           </div>
         </div>
-    </div>
+      </div>
     </section>
 
     <section class="section">
+      <div class="container">
+
+        <div class="content">
+          <p class="has-text-centered">Save the date! Die <strong>Nesselwang Competition</strong> findet statt am <strong>Samstag, 18.10.25</strong> an der <strong>Alpspitzbahn Nesselwang</strong>.</p>
+          <p class="has-text-centered"><a href="#da-simmer-dabei" class="button is-primary">Da will ich dabei sein!</a></p>
+        </div>
+      
+      </div>
+    </section>
+
+    <section class="section has-background-info">
       <div class="container">
         <!--
         <div class="notification is-primary">
@@ -104,8 +115,8 @@ useHead({
         <nav class="level">
           <div class="level-item has-text-centered">
             <div>
-              <p class="heading">Steigung bis zu</p>
-              <p class="title">30%</p>
+              <p class="heading">Anstrengung garantiert zu</p>
+              <p class="title">100%</p>
             </div>
           </div>
           <div class="level-item has-text-centered">
@@ -128,12 +139,43 @@ useHead({
           </div>
         </nav>
 
-        <div style="position:relative; width:100%; height:0px; padding-bottom:56.250%">
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+
+        <div class="content">
+          <p>Los geht's am Explorer Hotel Nesselwang, den Wasserfallweg entlang ein paar Treppen hinaus - bis man an der Mittelstation der Alpspitzbahn aus dem Wald bricht.
+          Dann geht's weiter auf der Teerstraße, durch die bekannte Bayernkurve und einen letzten Anstieg hinauf zur Bergstation.
+          Von dort aus ist es nur nur ein Katzensprung ans Sportheim Böck - das Ziel der Nesselwang Competition.</p>
+
+          <p>Schau's dir im Flyover an:</p>
+        </div>
+
+        <div class="mb-6" style="position:relative; width:100%; height:0px; padding-bottom:56.250%">
           <iframe allow="fullscreen" allowfullscreen height="100%"
             src="https://streamable.com/e/sksaz8?autoplay=0&loop=0" width="100%"
             style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;">
           </iframe>
         </div>
+
+        <div class="content">
+
+        <p>Du kannst entweder die ganze Strecke im <strong>Einzel</strong> alleine laufen, oder du entscheidest dich das Ganze in der <strong>2er Staffel</strong> zusammen mit einem/einer Anderen in Angriff zu nehmen - liegt ganz bei dir.</p>
+
+        <p>Bei der Staffel läuft die erste Person vom Start bis zur Mitte, die befindet sich an der Mittelstation der Alpspitzbahn.
+          Die zweite Person übernimmt dort und läuft den Rest der Strecke bis zum Sportheim Böck - dort befindet sich das Ziel.</p>
+
+        </div>
+
+      </div>
+    </section>
+
+    <section class="section" id="da-simmer-dabei">
+      <div class="container">
+
+        <p>Here goes the FORM</p>
 
       </div>
     </section>
