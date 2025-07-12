62a2a3caecebb27cee01e49db3797f804cf78b84
steffkes <stefan@gelungen.es>
2025-07-12 21:23:05 +0200

    allow zoom for impressions

--- pages/wettkampf/impressionen.vue
+++ pages/wettkampf/impressionen.vue
@@ -1,6 +1,22 @@
-<script setup></script>
+<script setup>
+const imageToShow = ref(false);
+const showThisImage = (event) => (imageToShow.value = event.target.src);
+</script>
 
 <template>
+  <div
+    class="modal"
+    :class="{ 'is-active': imageToShow }"
+    @click="imageToShow = false"
+  >
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <p class="image">
+        <img v-if="imageToShow" :src="imageToShow" />
+      </p>
+    </div>
+  </div>
+
   <section class="section">
     <div class="container is-max-desktop">
       <h1 class="title">Der erste Eindruck zählt</h1>
@@ -13,22 +29,22 @@
           <div class="fixed-grid has-2-cols">
             <div class="grid">
               <div class="cell">
-                <figure class="image is-4by3">
+                <figure @click="showThisImage" class="image is-4by3">
                   <img src="/images/ftn/ftn-01.jpg" />
                 </figure>
               </div>
               <div class="cell is-row-span-2">
-                <figure class="image is-3by4">
+                <figure @click="showThisImage" class="image is-3by4">
                   <img src="/images/ftn/ftn-02.jpg" />
                 </figure>
               </div>
               <div class="cell is-row-span-2">
-                <figure class="image is-3by4">
+                <figure @click="showThisImage" class="image is-3by4">
                   <img src="/images/ftn/ftn-03.jpg" />
                 </figure>
               </div>
               <div class="cell">
-                <figure class="image is-4by3">
+                <figure @click="showThisImage" class="image is-4by3">
                   <img src="/images/ftn/ftn-04.jpg" />
                 </figure>
               </div>
@@ -40,22 +56,22 @@
           <div class="fixed-grid has-2-cols">
             <div class="grid">
               <div class="cell">
-                <figure class="image is-4by3">
+                <figure @click="showThisImage" class="image is-4by3">
                   <img src="/images/ftn/ftn-06.jpg" />
                 </figure>
               </div>
               <div class="cell is-row-span-2">
-                <figure class="image is-3by4">
+                <figure @click="showThisImage" class="image is-3by4">
                   <img src="/images/ftn/ftn-05.jpg" />
                 </figure>
               </div>
               <div class="cell is-row-span-2">
-                <figure class="image is-3by4">
+                <figure @click="showThisImage" class="image is-3by4">
                   <img src="/images/ftn/ftn-07.jpg" />
                 </figure>
               </div>
               <div class="cell">
-                <figure class="image is-4by3">
+                <figure @click="showThisImage" class="image is-4by3">
                   <img src="/images/ftn/ftn-08.jpg" />
                 </figure>
               </div>
@@ -69,22 +85,22 @@
           <div class="fixed-grid has-2-cols">
             <div class="grid">
               <div class="cell">
-                <figure class="image is-4by3">
+                <figure @click="showThisImage" class="image is-4by3">
                   <img src="/images/ftn/ftn-09.jpg" />
                 </figure>
               </div>
               <div class="cell is-row-span-2">
-                <figure class="image is-3by4">
+                <figure @click="showThisImage" class="image is-3by4">
                   <img src="/images/ftn/ftn-11.jpg" />
                 </figure>
               </div>
               <div class="cell is-row-span-2">
-                <figure class="image is-3by4">
+                <figure @click="showThisImage" class="image is-3by4">
                   <img src="/images/ftn/ftn-12.jpg" />
                 </figure>
               </div>
               <div class="cell">
-                <figure class="image is-4by3">
+                <figure @click="showThisImage" class="image is-4by3">
                   <img src="/images/ftn/ftn-10.jpg" />
                 </figure>
               </div>
@@ -96,22 +112,22 @@
           <div class="fixed-grid has-2-cols">
             <div class="grid">
               <div class="cell">
-                <figure class="image is-4by3">
+                <figure @click="showThisImage" class="image is-4by3">
                   <img src="/images/ftn/ftn-13.jpg" />
                 </figure>
               </div>
               <div class="cell is-row-span-2">
-                <figure class="image is-3by4">
+                <figure @click="showThisImage" class="image is-3by4">
                   <img src="/images/ftn/ftn-14.jpg" />
                 </figure>
               </div>
               <div class="cell is-row-span-2">
-                <figure class="image is-3by4">
+                <figure @click="showThisImage" class="image is-3by4">
                   <img src="/images/ftn/ftn-15.jpg" />
                 </figure>
               </div>
               <div class="cell">
-                <figure class="image is-4by3">
+                <figure @click="showThisImage" class="image is-4by3">
                   <img src="/images/ftn/ftn-16.jpg" />
                 </figure>
               </div>
