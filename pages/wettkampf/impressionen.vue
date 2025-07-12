dde616b8bc4c15f9fe01a2de490f0d30d0986320
steffkes <stefan@gelungen.es>
2025-07-12 21:04:25 +0200

    use sommer pictures for impressions

--- pages/wettkampf/impressionen.vue
+++ pages/wettkampf/impressionen.vue
@@ -12,45 +12,107 @@
         <div class="column">
           <div class="fixed-grid has-2-cols">
             <div class="grid">
-              <div class="cell is-row-span-2">
-                <figure class="image is-3by4">
-                  <img src="/images/bild-01.jpg" />
-                </figure>
-              </div>
               <div class="cell">
                 <figure class="image is-4by3">
-                  <img src="/images/bild-02.jpg" />
+                  <img src="/images/ftn/ftn-01.jpg" />
                 </figure>
               </div>
               <div class="cell is-row-span-2">
                 <figure class="image is-3by4">
-                  <img src="/images/bild-04.jpg" />
+                  <img src="/images/ftn/ftn-02.jpg" />
+                </figure>
+              </div>
+              <div class="cell is-row-span-2">
+                <figure class="image is-3by4">
+                  <img src="/images/ftn/ftn-03.jpg" />
                 </figure>
               </div>
               <div class="cell">
                 <figure class="image is-4by3">
-                  <img src="/images/bild-03.jpg" />
+                  <img src="/images/ftn/ftn-04.jpg" />
                 </figure>
               </div>
             </div>
           </div>
         </div>
+
         <div class="column">
           <div class="fixed-grid has-2-cols">
             <div class="grid">
+              <div class="cell">
+                <figure class="image is-4by3">
+                  <img src="/images/ftn/ftn-06.jpg" />
+                </figure>
+              </div>
               <div class="cell is-row-span-2">
                 <figure class="image is-3by4">
-                  <img src="/images/bild-07.jpg" />
+                  <img src="/images/ftn/ftn-05.jpg" />
+                </figure>
+              </div>
+              <div class="cell is-row-span-2">
+                <figure class="image is-3by4">
+                  <img src="/images/ftn/ftn-07.jpg" />
                 </figure>
               </div>
               <div class="cell">
                 <figure class="image is-4by3">
-                  <img src="/images/bild-06.jpg" />
+                  <img src="/images/ftn/ftn-08.jpg" />
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns is-2">
+        <div class="column">
+          <div class="fixed-grid has-2-cols">
+            <div class="grid">
+              <div class="cell">
+                <figure class="image is-4by3">
+                  <img src="/images/ftn/ftn-09.jpg" />
+                </figure>
+              </div>
+              <div class="cell is-row-span-2">
+                <figure class="image is-3by4">
+                  <img src="/images/ftn/ftn-11.jpg" />
+                </figure>
+              </div>
+              <div class="cell is-row-span-2">
+                <figure class="image is-3by4">
+                  <img src="/images/ftn/ftn-12.jpg" />
                 </figure>
               </div>
               <div class="cell">
-                <figure class="image">
-                  <img src="/images/bild-05.jpg" />
+                <figure class="image is-4by3">
+                  <img src="/images/ftn/ftn-10.jpg" />
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="fixed-grid has-2-cols">
+            <div class="grid">
+              <div class="cell">
+                <figure class="image is-4by3">
+                  <img src="/images/ftn/ftn-13.jpg" />
+                </figure>
+              </div>
+              <div class="cell is-row-span-2">
+                <figure class="image is-3by4">
+                  <img src="/images/ftn/ftn-14.jpg" />
+                </figure>
+              </div>
+              <div class="cell is-row-span-2">
+                <figure class="image is-3by4">
+                  <img src="/images/ftn/ftn-15.jpg" />
+                </figure>
+              </div>
+              <div class="cell">
+                <figure class="image is-4by3">
+                  <img src="/images/ftn/ftn-16.jpg" />
                 </figure>
               </div>
             </div>
