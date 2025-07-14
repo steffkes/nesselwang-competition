122eb7bce1864d25e30eb070e76408c1e53d63a3
steffkes <stefan@gelungen.es>
2025-07-14 10:00:02 +0200

    initial rules
    based on oberstdorf

--- pages/wettkampf/regeln.vue
+++ pages/wettkampf/regeln.vue
@@ -32,6 +32,325 @@ useSeoMeta({
       </div>
     </div>
   </section>
+
+  <section class="section">
+    <div class="container is-max-desktop">
+      <div class="content">
+        <h2 id="regeln" class="mb-24">Wettkampfregeln</h2>
+
+        <div class="notification is-link">
+          <p>
+            Der Fire Trail Nesselwang wird in den Wettbewerben
+            <mark>Einzel</mark> und <mark>2er-Staffel</mark> durchgeführt. So
+            lang nicht explizit gekennzeichnet, gelten die nachfolgenden Regeln
+            für beide Wettbewerbe gleichermaßen.
+          </p>
+        </div>
+
+        <div class="notification has-background-warning-light">
+          <h3>Zeitüberschreitung</h3>
+
+          <ul>
+            <li>
+              <mark>Einzel</mark> Wenn der Läufer nach mehr als 60 (in Worten:
+              Sechzig) Minuten die Mittelstation der Alpspitzbahn erreicht,
+              <a href="#disqualifikation">wird er disqualifiziert</a>.
+            </li>
+            <li>
+              <mark>2er-Staffel</mark> Braucht einer der beiden Läufer für
+              seinen Abschnitt länger als 60 (in Worten: Sechszig) Minuten,
+              bekommt das Team 15 (in Worten: Fünfzehn) Minuten Zeitstrafe – und
+              zwar für jeden Abschnitt, bei dem die Zeit überschritten wurde.
+            </li>
+          </ul>
+        </div>
+
+        <h3>Atemschutzgeräte</h3>
+
+        <ul>
+          <li>
+            Alle Teams müssen Ihre Atemschutzgeräte selber mitbringen, egal ob
+            Normal- oder Überdruck bzw. Stahl- oder Compositebehälter.
+          </li>
+          <li>Maske und Lungenautomat werden nicht benötigt!</li>
+        </ul>
+
+        <h3>Bekleidung</h3>
+
+        <ul>
+          <li>
+            Gestartet wird mit Einsatzstiefeln (nach DIN EN 345-2 / S3 oder
+            gleichwertig)
+          </li>
+          <li>
+            Einsatzhose und –jacke (Feuerwehrschutzkleidung nach DIN EN 469,
+            <em>für den Innenangriff geeignet</em>)
+          </li>
+          <li>Feuerwehreinsatzhelm</li>
+          <li>Atemschutzgerät auf dem Rücken</li>
+          <li>
+            <mark style="background-color: #f0f0f0"
+              ><abbr title="Technisches Hilfswerk">THW</abbr> abweichend</mark
+            >
+            Es ist der vollständige Multifunktionale Einsatzschutzanzug (MEA)
+            (Variante Atemschutzgeräteträger) <strong>mit</strong> Innenhose zu
+            tragen. (gemäß
+            <abbr
+              title="Richtlinie über die Bekleidung und Kennzeichnung im Technischen Hilfswerk"
+              >BeklRiLi</abbr
+            >
+            2.1.7 & 1.1 Anlage 1)
+          </li>
+        </ul>
+        <p>Die Ausrüstung wird vor dem Start vom Veranstalter überprüft.</p>
+
+        <h3 id="disqualifikation">Disqualifikation</h3>
+
+        <ul>
+          <li>
+            <mark>Einzel</mark>
+            Wenn der Läufer nach mehr als 60 (in Worten: Sechzig) Minuten die
+            Mittelstation der Alpspitzbahn erreicht.
+          </li>
+          <li>Nichteinhaltung der Wettkampfregeln</li>
+          <li>Unsportliches Verhalten</li>
+          <li>Behinderung der anderen Teilnehmer</li>
+          <li>Manipulation an Schutzausrüstung, Atemschutzgerät</li>
+          <li>
+            Wenn verlorengegangene Ausrüstungsgegenstände nicht selbstständig
+            aufgehoben werden
+          </li>
+          <li>Wenn von der vorgegebenen Wettkampfstrecke abgewichen wird</li>
+          <li>
+            Wenn ein Teilnehmer Teile der Wettkampfausrüstung während des
+            Wettkampfes ablegt.
+          </li>
+        </ul>
+
+        <p>
+          Bei Disqualifikation (egal ob vor, während oder nach dem Wettkampf)
+          wird die Startgebühr als Aufwandsentschädigung einbehalten.
+        </p>
+
+        <h3>G26.3</h3>
+
+        <ul>
+          <li>
+            Ist nicht notwendig. Das Atemschutzgerät wird nur geschultert.
+          </li>
+        </ul>
+
+        <h3>Feuerwehrhaltegurt</h3>
+
+        <ul>
+          <li>Feuerwehrhaltegurt (auch Koppel genant) ist nicht gestattet.</li>
+        </ul>
+
+        <h3>Flammschutzhaube</h3>
+
+        <ul>
+          <li>Ist nicht notwendig.</li>
+        </ul>
+
+        <h3>Haftungsausschluss</h3>
+
+        <ul>
+          <li>
+            Der <a href="#haftungsausschluss">Haftungsausschluss</a> muss zum
+            Abschließen der Anmeldung anerkannt werden.
+          </li>
+        </ul>
+
+        <h3>Helmkameras</h3>
+
+        <ul>
+          <li>
+            Helmkameras dürfen nur dann benutzt werden, wenn sie mit der
+            entsprechend dafür vorgesehenen Halterung so fest am Helm angebracht
+            sind, dass sie gegen "abfallen" gesichert sind.
+          </li>
+          <li>Halterungen der Marke "Eigenbau" sind nicht gestattet.</li>
+          <li>
+            Kameras mit Brustgurt (z.B. GoPro) dürfen benutzt werden, müssen
+            aber auch nochmal, z.B. durch eine kurze Bandschlinge, gegen
+            "abfallen" gesichert werden!
+          </li>
+          <li>
+            Alle „Anbauten“ werden vor dem Start vom Veranstalter überprüft:
+            überstehen sie den „Wackeltest“ nicht, müssen sie abgenommen werden!
+          </li>
+        </ul>
+
+        <h3>Helmlampen</h3>
+
+        <ul>
+          <li>Sind nicht notwendig.</li>
+        </ul>
+
+        <h3>Maske</h3>
+
+        <ul>
+          <li>Eine Maske <strong>darf nicht</strong> getragen werden.</li>
+        </ul>
+
+        <h3>Musik</h3>
+
+        <ul>
+          <li>
+            Wer beim laufen Musik über Kopfhörer hören möchte, kann dies tun.
+          </li>
+          <li>
+            Wiedergabegerät bitte so verstauen, dass es nicht rausfallen kann.
+          </li>
+          <li>Verlorene Gegenstände müssen eingesammelt werden!</li>
+          <li>
+            <strong>Angepasste Lautstärke</strong>: Wenn euch jemand einen
+            Überholvorgang ankündigt, solltet ihr dies wahrnehmen können
+          </li>
+        </ul>
+
+        <h3>Startmodus</h3>
+
+        <ul>
+          <li>
+            <mark>2er-Staffel</mark> Der zweite Läufer startet spätestens 45 (in
+            Worten: fünfundvierzig) Minuten nach dem ersten Läufer. Falls der
+            erste Läufer binnen dieser Zeit an der Mittelstation ankommt,
+            erfolgt eine klassische Staffelübergabe..
+          </li>
+        </ul>
+
+        <h3>Überholen</h3>
+
+        <ul>
+          <li>
+            Ist aufgrund der Strecke möglich & wahrscheinlich. Bitte lasst
+            schnellere Teilnehmer vorbei.
+          </li>
+        </ul>
+
+        <h3>Zubehör</h3>
+
+        <ul>
+          <li>
+            Alle „Anbauten“ werden vor dem Start überprüft … überstehen sie den
+            „Wackeltest“ nicht, müssen sie abgenommen werden!
+          </li>
+        </ul>
+      </div>
+
+      <p class="mt-2 has-text-grey-light">
+        Diese Wettkampfregeln sind angelehnt an die Regeln des
+        <a href="http://www.schanzenlauf-oberstdorf.de"
+          >Schanzenlauf Oberstdorf</a
+        >
+        und wurden entsprechend erweitert bzw. angepasst.
+      </p>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container is-max-desktop">
+      <div class="content">
+        <h2 id="haftungsausschluss">Haftungsausschluss</h2>
+        <h3>Haftung</h3>
+
+        <p>
+          Der Veranstalter übernimmt gegenüber den Teilnehmenden und dritten
+          Personen keine Haftung.
+        </p>
+
+        <h3>Teilnehmererklärung</h3>
+
+        <p>
+          Ohne jegliche Einschränkung der Verantwortlichkeit eines Veranstalters
+          hinsichtlich der Durchführung des Wettkampfes gebe ich nachstehende
+          Erklärung ab:
+        </p>
+
+        <ol>
+          <li>
+            <strong>Bezeichnung der Risiken</strong>: Ich weiß und bin mir voll
+            der Gefahren bewusst, welche die Ausübung des oben genannten
+            Wettkampfes beinhaltet. Ich erkenne, dass mit Anstreben
+            ausgezeichneter Leistungen ein Risiko verbunden ist, welches darin
+            besteht, dass ich meine physischen Fähigkeiten bis zum absoluten
+            Limit erstrecken muss. Ich weiß und akzeptiere, dass mit der
+            Ausübung eines solchen Wettkampfsportes Leben und körperliche
+            Sicherheit gefährdet sein können. Diese Gefahren drohen jedermann im
+            Wettkampf- und Trainingsbereich, insbesondere aus den
+            Umweltbedingungen, technischen Ausrüstungen, atmosphärischen
+            Einflüssen sowie natürlichen oder künstlichen Hindernissen. Ich bin
+            mir bewusst, dass gewisse Abläufe nicht immer vorausgesehen oder
+            unter Kontrolle gehalten und daher auch nicht ausgeschaltet oder
+            durch Sicherheitsvorkehrungen präventiv verhindert werden können.
+            Die Teilnahme am Wettkampf erfolgt auf eigenes Risiko. Die Haftung
+            des Veranstalters und der Wettkampfleitung gegenüber den
+            Wettkampfteilnehmern wird auf Vorsatz und grobe Fahrlässigkeit
+            begrenzt. Die darüber hinausgehende Haftung ist ausgeschlossen.
+          </li>
+
+          <li>
+            <strong>Risikobereitschaft</strong>: Ich muss selbst beurteilen, ob
+            die Wettkampf- oder Trainingsstrecke nach den gegebenen
+            Verhältnissen für mich nicht zu schwierig ist. Ich erkläre, dass ich
+            offensichtliche Sicherheitsmängel unverzüglich der Rennleitung
+            melden werde.
+          </li>
+
+          <li>
+            <strong>Persönliche Haftung</strong>: Ich bin mir bewusst, dass ich
+            persönlich gegenüber Drittpersonen für Schäden zufolge
+            Körperverletzungen oder Sachbeschädigung, welche auf meine Teilnahme
+            am Training oder am Wettkampf zurückzuführen sind, haftbar sein
+            kann. Ich erkenne an, dass es nicht der Verantwortung des
+            Veranstalters unterliegt, meine Ausrüstung zu prüfen oder zu
+            überwachen. Ich erkläre, mich mit den jeweiligen
+            Wettkampfbestimmungen vertraut zu machen. Ich bestätige, den
+            vorstehende Haftungsausschluss gelesen zu haben.
+          </li>
+          <li>
+            <strong>Startgeld</strong>: Bei Nichtantritt eines Teilnehmers, oder
+            bei Ausfall wegen höherer Gewalt besteht kein Anspruch auf
+            Rückerstattung des Startgeldes.
+          </li>
+        </ol>
+
+        <h3>Unverbindliche Information zum Unfallversicherungsschutz</h3>
+
+        <h4>Freiwillige Feuerwehr</h4>
+
+        <p>
+          Für Angehörige der Freiwilligen Feuerwehr einschließlich der
+          Jugendfeuerwehr besteht üblicherweise der eigene gesetzliche
+          Unfallversicherungsschutz, sofern sie von ihrem zuständigen
+          Kommandanten für den Fire Trail Nesselwang im Rahmen ihres
+          Feuerwehrdienstes entsandt worden sind.
+        </p>
+
+        <h4>Werk- & Betriebsfeuerwehr</h4>
+
+        <p>
+          Angehörige von Werk- & Betriebsfeuerwehren sind üblicherweise dann
+          selbst unfallversichert, wenn ihre Teilnahme im Rahmen ihrer
+          beruflichen Tätigkeit gemäß § 2 Abs. 1 Nr. 1 SGB VII erfolgt.
+          Zuständiger Unfallversicherungsträger ist die jeweilige
+          Fachberufsgenossenschaft, bei der das Unternehmen Mitglied ist. Es
+          wird empfohlen, den Versicherungsschutz rechtzeitig über den
+          Arbeitgeber zu klären.
+        </p>
+
+        <h4>Berufsfeuerwehr</h4>
+
+        <p>
+          Beamtinnen und Beamte der Berufsfeuerwehr sind üblicherweise dann
+          selbst unfallversichert, wenn ein Unfall anlässlich des Fire Trail
+          Nesselwang als Dienstunfall im Sinne des Beamtenversorgungsgesetzes
+          anzuerkennen ist.
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
