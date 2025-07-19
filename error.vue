1ed356a534f015f74466dd452c77b84bc67660da
steffkes <stefan@gelungen.es>
2025-07-19 13:47:55 +0200

    set up error page

---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<template>
+  <NuxtLayout name="default">
+    <div>
+      <h2>Fehler {{ error?.statusCode }}</h2>
+      <button @click="handleError">Bring mich zur Startseite</button>
+      <p v-if="error?.statusMessage">{{ error.statusMessage }}</p>
+    </div>
+  </NuxtLayout>
+</template>
