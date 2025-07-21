43552540900aa6c006a87b2b44a4a4b597398520
steffkes <stefan@gelungen.es>
2025-07-21 22:56:59 +0200

    add missing component for destinations

---
+++ components/ImageSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="mt-6 fixed"
+    :style="{
+      'min-height': '40vh',
+      'background-image': 'url(' + url + ')',
+      'background-size': 'cover',
+      'background-position':
+        (position.x ?? '50') + '% ' + (position.y ?? '50') + '%',
+    }"
+  ></div>
+
+  <section class="section">
+    <div class="container is-max-desktop">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.fixed {
+  background-attachment: fixed;
+}
+
+@supports (-webkit-touch-callout: inherit) {
+  .fixed {
+    background-attachment: scroll;
+  }
+}
+</style>
+
+<script setup>
+defineProps(["url", "position"]);
+</script>
