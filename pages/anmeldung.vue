8891a0af67e6dd6c4708827ca658038791f2a5c4
steffkes <stefan@gelungen.es>
2025-07-21 17:11:46 +0200

    explain pricing on register page
    it's not so obvious otherwise, maybe place this information on
    other pages as well?

--- pages/anmeldung.vue
+++ pages/anmeldung.vue
@@ -3,6 +3,10 @@
 <template>
   <section class="section">
     <div class="container is-max-desktop">
+      <p class="mb-4">
+        Teilnahme als Einzel kostet 40 €, Teilnahme als 2er-Staffel kostet 90€.
+      </p>
+
       <iframe
         src="https://events.raceresult.com/registrations/?event=343456&n=Einzel_Anmeldung&k=lOvTMKQARkrW"
         width="100%"
