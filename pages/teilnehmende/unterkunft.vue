ee2b92fe7cbcf6a5c436234522f859544bf599f4
steffkes <stefan@gelungen.es>
2026-04-02 16:48:37 +0200

    re-add hotel offer at explorer

--- pages/teilnehmende/unterkunft.vue
+++ pages/teilnehmende/unterkunft.vue
@@ -14,11 +14,96 @@ useSeoMeta({
         etwas dabei
       </p>
 
-      <div class="notification">
+      <p class="title is-4 mt-6">Explorer Hotel Nesselwang</p>
+      <p class="subtitle is-6">Übernachtung & Frühstück</p>
+
+      <div class="notification is-info">
         <p>
-          Der 1. Fire Trail Nesselwang fand am 18.10.2025 statt. Deswegen gibt
-          es aktuell hier keine Infos.
+          <strong>Buchungsstichwort: „Feuerwehr“</strong><br />
+          📞 <a href="tel:004983619989000">+49 (8361) 998 - 9000</a><br />
+          ✉️
+          <a href="mailto:neuschwanstein@explorer-hotels.com"
+            >neuschwanstein@explorer-hotels.com</a
+          ><br />
         </p>
+      </div>
+
+      <div class="content">
+        <p>Inklusivleistungen:</p>
+        <ul>
+          <li>
+            Übernachtung in Design-Zimmern (21m²) mit Dusche/WC, Flatscreen-TV
+          </li>
+          <li>vitales Frühstücksbuffet</li>
+          <li>
+            Nutzung des Sport-Spa mit Sauna, Dampfbad, Infrarotkabine und
+            Fitnessraum
+          </li>
+          <li>Gratis WLAN-Nutzung im gesamten Hotel</li>
+        </ul>
+        <p>
+          Der Preis inklusive der genannten Leistungen beträgt vom 10.10. –
+          11.10.2026:
+        </p>
+        <ul>
+          <li>€ 79,80 pro Person pro Übernachtung im Doppelzimmer</li>
+          <li>
+            € 124,80 pro Person pro Übernachtung im Doppelzimmer zur
+            Einzelnutzung
+          </li>
+        </ul>
+
+        <p>
+          Eine zusätzliche Übernachtung von 09.10. - 10.10. ist auf Anfrage
+          möglich. Der Kurbeitrag wird separat berechnet (z. Zt. € 2,10 pro
+          Person pro Nacht).
+        </p>
+
+        <p>Parkplatz:</p>
+        <ul>
+          <li>Außenstellplatz: € 1,00 pro PKW pro Nacht</li>
+          <li>Tiefgaragenstellplatz: € 8,80 pro PKW pro Nacht</li>
+        </ul>
+
+        <p>
+          Explorer Hotels sind eine sportliche und nachhaltige Hotelgruppe mit
+          11 Standorten in den deutschsprachigen Alpen. Ausgerichtet auf
+          bergaffine Gäste, setzen die Explorer Hotels vor allem im Bereich
+          Nachhaltigkeit einen großen Fokus.
+        </p>
+      </div>
+
+      <div class="grid is-col-min-12">
+        <div class="cell">
+          <figure class="image">
+            <img src="/images/explorer-hotel/explorer-hotel.jpg" />
+          </figure>
+        </div>
+        <div class="cell">
+          <figure class="image">
+            <img src="/images/explorer-hotel/zimmer.jpg" />
+          </figure>
+        </div>
+        <div class="cell">
+          <figure class="image">
+            <img src="/images/explorer-hotel/eingang.jpg" />
+          </figure>
+        </div>
+        <div class="cell">
+          <figure class="image">
+            <img src="/images/explorer-hotel/fruehstueck.jpg" />
+          </figure>
+        </div>
+        <div class="cell">
+          <figure class="image">
+            <img src="/images/explorer-hotel/info.jpg" />
+          </figure>
+        </div>
+        <div class="cell">
+          <figure class="image">
+            <img src="/images/explorer-hotel/bike.jpg" />
+          </figure>
+        </div>
       </div>
     </div>
   </section>
